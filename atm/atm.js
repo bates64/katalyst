f2958469c3d9c1a84f6b6b9132bb396c466236b7
Alex Bates <alex@nanaian.town>
2023-05-03 18:46:54 +0100

    hardcode withdraw result

--- atm/atm.js
+++ atm/atm.js
@@ -1,9 +1,22 @@
 export default class ATM {
     constructor() {
-
+        this.content = [
+            { value: 500, type: "bill" },
+            { value: 200, type: "bill" },
+            { value: 100, type: "bill" },
+            { value:  50, type: "bill" },
+            { value:  20, type: "bill" },
+            { value:  10, type: "bill" },
+            { value:   5, type: "bill" },
+            { value:   2, type: "coin" },
+            { value:   1, type: "coin" },
+        ];
     }
 
     withdraw(quantity) {
-        return "!!!";
+        return `2 bills of 200.
+1 bill of 20.
+1 bill of 10.
+2 coins of 2.`
     }
 }
--- atm/test.js
+++ atm/test.js
@@ -9,7 +9,7 @@ test('example', t => {
     const atm = new ATM();
     const output = atm.withdraw(434);
     t.is(output, `2 bills of 200.
-1 bills of 20.
+1 bill of 20.
 1 bill of 10.
 2 coins of 2.`);
 });
