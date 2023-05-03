759aea9eb01c5eb20c807f0066943a34e5d4b22e
Alex Bates <alex@nanaian.town>
2023-05-03 18:43:37 +0100

    add example test and basic interface

---
+++ atm/atm.js
@@ -0,0 +1,9 @@
+export default class ATM {
+    constructor() {
+
+    }
+
+    withdraw(quantity) {
+        return "!!!";
+    }
+}
--- atm/test.js
+++ atm/test.js
@@ -1,5 +1,15 @@
 import test from 'ava';
+import ATM from "./atm.js";
 
 test('1+1=2', t => {
     t.is(1 + 1, 2);
+});
+
+test('example', t => {
+    const atm = new ATM();
+    const output = atm.withdraw(434);
+    t.is(output, `2 bills of 200.
+1 bills of 20.
+1 bill of 10.
+2 coins of 2.`);
 });
