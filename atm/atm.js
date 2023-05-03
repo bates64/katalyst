d34c46f70bcd12cfa0d712eacf55cee8125558f5
Alex Bates <alex@nanaian.town>
2023-05-03 19:24:09 +0100

    iteration 2

--- atm/atm.js
+++ atm/atm.js
@@ -1,15 +1,15 @@
 export default class ATM {
     constructor() {
         this.content = [
-            { value: 500, type: "bill" },
-            { value: 200, type: "bill" },
-            { value: 100, type: "bill" },
-            { value:  50, type: "bill" },
-            { value:  20, type: "bill" },
-            { value:  10, type: "bill" },
-            { value:   5, type: "bill" },
-            { value:   2, type: "coin" },
-            { value:   1, type: "coin" },
+            { value: 500, type: "bill", count:   2 },
+            { value: 200, type: "bill", count:   3 },
+            { value: 100, type: "bill", count:   5 },
+            { value:  50, type: "bill", count:  12 },
+            { value:  20, type: "bill", count:  20 },
+            { value:  10, type: "bill", count:  50 },
+            { value:   5, type: "bill", count: 100 },
+            { value:   2, type: "coin", count: 250 },
+            { value:   1, type: "coin", count: 500 },
         ];
     }
 
@@ -18,14 +18,21 @@ export default class ATM {
 
         while (quantity > 0) {
             const largestItem = this.findBestItemForQuantity(quantity);
-            if (!largestItem) throw new Error("no item for quantity");
+            if (!largestItem)
+                throw new Error("The ATM machine has not enough money, please go to the nearest atm machine");
 
-            // Subtract and output
-            const count = Math.floor(quantity / largestItem.value);
-            quantity = quantity % largestItem.value;
+            // Calculate how many units we need
+            let count = Math.floor(quantity / largestItem.value);
+
+            // Limit count to however many units we have
+            if (count > largestItem.count) count = largestItem.count;
+
+            // Update
+            largestItem.count -= count; 
+            quantity -= largestItem.value * count;
             result.push({
-                count,
                 ...largestItem,
+                count,
             });
         }
 
@@ -36,7 +43,7 @@ export default class ATM {
     findBestItemForQuantity(quantity) {
         for (const item of this.content) {
             //console.log(item.value, "<=", quantity)
-            if (item.value <= quantity) {
+            if (item.value <= quantity && item.count > 0) {
                 return item;
             }
         }
--- atm/test.js
+++ atm/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import ATM from "./atm.js";
 
-test('example', t => {
+test('withdraw 434', t => {
     const atm = new ATM();
     const output = atm.withdraw(434);
     t.deepEqual(output, [
@@ -12,14 +12,66 @@ test('example', t => {
     ]);
 });
 
+test('withdraw 1725', t => {
+    const atm = new ATM();
+    const output = atm.withdraw(1725);
+    t.deepEqual(output, [
+        { value: 500, type: 'bill', count: 2 },
+        { value: 200, type: 'bill', count: 3 },
+        { value: 100, type: 'bill', count: 1 },
+        { value:  20, type: 'bill', count: 1 },
+        { value:   5, type: 'bill', count: 1 },
+    ]);
+    t.deepEqual(atm.content, [
+        { value: 500, type: 'bill', count:    0 },
+        { value: 200, type: 'bill', count:    0 },
+        { value: 100, type: 'bill', count:    4 },
+        { value:  50, type: 'bill', count:   12 },
+        { value:  20, type: 'bill', count:   19 },
+        { value:  10, type: 'bill', count:   50 },
+        { value:   5, type: 'bill', count:   99 },
+        { value:   2, type: 'coin', count:  250 },
+        { value:   1, type: 'coin', count:  500 },
+    ]);
+});
+
+test('withdraw 1725 then 1825', t => {
+    const atm = new ATM();
+    atm.withdraw(1725);
+    const output = atm.withdraw(1825);
+    t.deepEqual(output, [
+        { value: 100, type: 'bill', count:  4 },
+        { value:  50, type: 'bill', count: 12 },
+        { value:  20, type: 'bill', count: 19 },
+        { value:  10, type: 'bill', count: 44 },
+        { value:   5, type: 'bill', count: 1 },
+    ]);
+    t.deepEqual(atm.content, [
+        { value: 500, type: 'bill', count:    0 },
+        { value: 200, type: 'bill', count:    0 },
+        { value: 100, type: 'bill', count:    0 },
+        { value:  50, type: 'bill', count:    0 },
+        { value:  20, type: 'bill', count:    0 },
+        { value:  10, type: 'bill', count:    6 },
+        { value:   5, type: 'bill', count:   98 },
+        { value:   2, type: 'coin', count:  250 },
+        { value:   1, type: 'coin', count:  500 },
+    ]);
+});
+
+test('withdrawing too much errors', t => {
+    const atm = new ATM()
+    t.throws(() => atm.withdraw(99999));
+});
+
 test('findBestItemForQuantity(434) == 200', t => {
     const atm = new ATM()
-    t.deepEqual(atm.findBestItemForQuantity(434), { value: 200, type: 'bill' });
+    t.deepEqual(atm.findBestItemForQuantity(434), { value: 200, type: 'bill', count: 3 });
 });
 
 test('findBestItemForQuantity(34) == 20', t => {
     const atm = new ATM()
-    t.deepEqual(atm.findBestItemForQuantity(34), { value: 20, type: 'bill' });
+    t.deepEqual(atm.findBestItemForQuantity(34), { value: 20, type: 'bill', count: 20 });
 });
 
 test('findBestItemForQuantity(0) == null', t => {
